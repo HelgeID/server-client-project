4a118cd2341060d721eb8f15a4c995b553221f06
HelgeID <34999595+HelgeID@users.noreply.github.com>
2020-07-09 09:33:39 +0300

    connection many clients to server

--- sourceServer.cpp
+++ sourceServer.cpp
@@ -5,6 +5,20 @@
 
 #pragma warning(disable: 4996)
 
+SOCKET arrConnections[100]{ 0 }; //масив сокетів
+int counter = 0; //індекс з'єднання
+
+void ClientHandler(int index)
+{
+	//чекаємо від'єднання клієнта
+	if (recv(arrConnections[index], NULL, 0, 0) != 0)
+		std::cout << "Client " << index << " disconnected, ";
+	counter == 0 ? counter : counter--;
+	std::cout << "counter: " << counter << std::endl;
+	arrConnections[index] = 0;
+	return;
+}
+
 int main()
 {
 	//загружаємо бібліотеку
@@ -29,17 +43,28 @@ int main()
 	//прослушка по порту sListen, SOMAXCONN - максимальне число запитів
 	listen(sListen, SOMAXCONN);
 
-	std::cout << "Server loaded, waiting for clients...\n";
+	std::cout << "Server loaded, waiting for Clients...\n";
 
 	//сокет, по-якому отримаємо з'єднання з клієнтом
 	SOCKET newConnection;
-	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeof_addr); //return ptr на новий сокет
 
-	//перевірка з'єднання
-	if (newConnection != 0)
-		std::cout << "Client connected!" << std::endl;
-	else
-		std::cout << "Error connection!" << std::endl;
+	while (true) {
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeof_addr);
+
+		if (newConnection != 0) { //перевірка з'єднання
+			for (int i(0); i < 100; ++i) { //надаємо вільний id
+				if (arrConnections[i] == 0) {
+					arrConnections[i] = newConnection;
+					counter++; //загальне число клієнтів
+					std::cout << "new Client connected!" << std::endl;
+					CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientHandler, (LPVOID)(i), NULL, NULL);
+					break;
+				}
+			}
+
+		}
+
+	}
 
 	system("pause");
 	return 0;
